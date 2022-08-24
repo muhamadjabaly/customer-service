663690162744cd8050f452c8e78ae0fd9e918018
Muhammad Jabali <hamafantasy11@gmail.com>
2022-08-24 15:00:59 +0300

    notification service

--- customer/src/main/java/com/demo/customer/CustomerService.java
+++ customer/src/main/java/com/demo/customer/CustomerService.java
@@ -2,6 +2,8 @@ package com.demo.customer;
 
 import com.demo.clients.fraud.FraudCheckResponse;
 import com.demo.clients.fraud.FraudClient;
+import com.demo.clients.notification.NotificationClient;
+import com.demo.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,10 +13,11 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final NotificationClient notificationClient;
     private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
+        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
         // todo: check if email valid
         // todo: check if email not taken
@@ -25,9 +28,15 @@ public class CustomerService {
 //                FraudCheckResponse.class,
 //                customer.getId()
 //        );
+
         FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
-        if(fraudCheckResponse.isFraudster()){
+        if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("fraudster");
         }
+        notificationClient.sendNotification(
+                new NotificationRequest(customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to Amigoscode...",
+                                customer.getFirstName())));
     }
 }
